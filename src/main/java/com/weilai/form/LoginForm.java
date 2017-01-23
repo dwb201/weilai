66360dc623905dd2912a8f9c7152546863a2abba
dwb <dwb201@hotmail.com>
2017-01-24 08:35:51 +0900

    backup

---
+++ src/main/java/com/weilai/form/LoginForm.java
@@ -0,0 +1,27 @@
+package com.weilai.form;
+
+public class LoginForm
+{
+  private String login_user;
+  private String login_pass;
+  
+  public String getLogin_user()
+  {
+    return this.login_user;
+  }
+  
+  public void setLogin_user(String login_user)
+  {
+    this.login_user = login_user;
+  }
+  
+  public String getLogin_pass()
+  {
+    return this.login_pass;
+  }
+  
+  public void setLogin_pass(String login_pass)
+  {
+    this.login_pass = login_pass;
+  }
+}
